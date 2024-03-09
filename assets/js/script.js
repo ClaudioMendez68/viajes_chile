25fbc29e7fad3387ce343192a1c891393fd94efb
Claudio Mendez <claudio.mendez.online@gmail.com>
2024-03-09 15:33:57 -0300

    Se agregan funcionalidades hover y click al navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,38 @@
-// Smooth Scroll
 $(document).ready(function () {
-    $("a").on("click", function (event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        $("html, body").animate(
-          {
-            scrollTop: $(hash).offset().top,
-          },
-          100,
-          function () {
-            window.location.hash = hash;
-          }
-        );
-      }
-    });
+  // Hover en el navbar
+  $(".nav-link").hover(
+    function () {
+      $(this).addClass("text-info");
+    },
+    function () {
+      $(this).removeClass("text-info");
+    }
+  );
+
+  // Click en el navbar
+  $(".nav-link").click(function () {
+    $(".nav-link").removeClass("text-black");
+    $(this).addClass("text-black");
   });
+
+  $(".navbar-brand").click(function () {
+    $(".nav-link").removeClass("text-black");
+  });
+
+  // Smooth Scroll
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        100,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
